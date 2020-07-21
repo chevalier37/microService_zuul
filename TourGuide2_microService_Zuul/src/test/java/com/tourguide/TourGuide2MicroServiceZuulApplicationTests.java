f9e1d7859fa8dba4c68ac4631b82019cb292768c
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-07-21 16:18:23 +0200

    initial commit

---
+++ TourGuide2_microService_Zuul/src/test/java/com/tourguide/TourGuide2MicroServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourGuide2MicroServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
